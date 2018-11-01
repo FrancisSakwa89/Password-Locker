4c89ed3420510635d04818de4a04de4dcc8be691
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-11-01 16:05:33 +0300

    added a class password to  password.py file

---
+++ password.py
@@ -0,0 +1,16 @@
+class Password:
+    """
+    Class that generates new instances of passwords
+    """
+
+    pass
+
+def _init_(self,first_name,last_name,password_input):
+    '''
+    _init_method that helps us define properties for our objects.
+
+    Argvs:
+       first_name:New password first name.
+       last_name:New password last name.
+       password:New password password input.
+    '''
