c23cda9c7d32f6479464ae2f07d03a74444c8a76
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-11-01 16:12:11 +0300

    added an empty password list in class password.py

--- password.py
+++ password.py
@@ -3,14 +3,12 @@ class Password:
     Class that generates new instances of passwords
     """
 
-    pass
+    password_list = [] #Empty password list
 
-def _init_(self,first_name,last_name,password_input):
-    '''
-    _init_method that helps us define properties for our objects.
+    def _init_(self,first_name,last_name,password_input):
+      # docstring removed for simplicity
 
-    Argvs:
-       first_name:New password first name.
-       last_name:New password last name.
-       password:New password password input.
-    '''
+       self.first_name:New password first name.
+       self.last_name:New password last name.
+       self.password:New password password input.
+    
