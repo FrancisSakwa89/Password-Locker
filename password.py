8152a700e71255c1d4b03771799a0de7eb651cd8
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-11-02 09:32:05 +0300

    done the the third test of saving multiple passwords

--- password.py
+++ password.py
@@ -12,3 +12,26 @@ class Password:
        self.last_name = last_name
        self.password_input = password_input
     
+      # Init method up here
+    def save_password(self):
+
+        '''
+        save_password method saves password objects into password_list
+        '''
+
+        Password.password_list.append(self)
+
+      # Items up here...
+
+    def test_save_multiple_password(self):
+        '''
+        test_save_multiple_passowrd to check if we can save multiple password
+        objects to our password_list
+        '''
+        self.new_passowrd.save_passowrd()
+        test_password = Password("first_name","last_name","password_input") # new password
+        test_password.save_password()
+        self.assertEqual(len(Password.password_list),2)
+
+if __name__ == '__main__':
+    unittest.main()    
